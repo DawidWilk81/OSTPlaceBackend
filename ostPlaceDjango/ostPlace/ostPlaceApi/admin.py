079d8623e6f1291cbf71594a187e86b92bd4a8c6
Dawid Wilk <38471575+DawidWilk81@users.noreply.github.com>
2023-03-01 12:05:16 +0100

    refresh

--- ostPlaceDjango/ostPlace/ostPlaceApi/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Song, Tag, BasketOST, UserAccount
-# Register your models here.
-
-admin.site.register(Tag)
-admin.site.register(Song)
-admin.site.register(BasketOST)
-admin.site.register(UserAccount)
-
--- ostPlaceDjango/ostPlace/ostPlaceApi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OstplaceapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ostPlaceApi'
